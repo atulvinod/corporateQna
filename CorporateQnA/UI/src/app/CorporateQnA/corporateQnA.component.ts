59bd5338340f2ed379c18245bdf892e52dd818e3
atulvinod <atulvinod1911@gmail.com>
2021-01-23 17:19:05 +0530

    Created Top navbar and left side nav column

---
+++ CorporateQnA/UI/src/app/CorporateQnA/corporateQnA.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+@Component
+    ({
+        selector: "corporate-qna",
+        templateUrl:"./corporateQnA.component.html"
+    })
+export class CorporateQnAComponent{
+    constructor() {}
+}
