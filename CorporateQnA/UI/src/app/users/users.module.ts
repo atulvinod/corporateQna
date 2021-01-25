c8682d76040cb471b69f57d4fa53f690766a0c29
atulvinod <atulvinod1911@gmail.com>
2021-01-25 15:27:51 +0530

    Created Users Module

---
+++ CorporateQnA/UI/src/app/users/users.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from "@angular/core";
+import { UsersRoutingModule } from "./users.routing";
+
+@NgModule({
+    imports:[
+        UsersRoutingModule
+    ],
+    declarations:[
+
+    ],
+    exports:[
+        
+    ]
+})
+export class UsersModule{}
