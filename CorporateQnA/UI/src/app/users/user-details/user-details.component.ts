4cfc20efa1b1ed9f27549b2fce6a5ca1e182ced2
atulvinod <atulvinod1911@gmail.com>
2021-01-25 16:40:55 +0530

    Added question componet to the user details page created grid in user details page for layout Created user details page header

--- CorporateQnA/UI/src/app/users/user-details/user-details.component.ts
+++ CorporateQnA/UI/src/app/users/user-details/user-details.component.ts
@@ -1,14 +1,17 @@
-import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { faSearch, faArrowLeft, faThumbsDown, faThumbsUp } from '@fortawesome/free-solid-svg-icons';
 import { FormGroup, FormControl } from '@angular/forms';
 import { Component } from '@angular/core';
 @Component
     ({
         selector: "app-user-details",
-        templateUrl:"./user-details.component.html"
+        templateUrl: "./user-details.component.html"
     })
-export class UserDetailsComponent{
+export class UserDetailsComponent {
 
     searchForm: FormGroup;
+    faArrowLeft = faArrowLeft
+    thumbsUp = faThumbsUp
+    thumbsDown = faThumbsDown
     
     constructor() {
         this.searchForm = new FormGroup({
--- CorporateQnA/UI/src/app/users/users.routing.ts
+++ CorporateQnA/UI/src/app/users/users.routing.ts
@@ -14,7 +14,7 @@ const routes : Routes = [
                 component: AllUsersComponent
             },
             {
-                path:"view/:id",
+                path:"view",
                 component: UserDetailsComponent
             }
         ]
