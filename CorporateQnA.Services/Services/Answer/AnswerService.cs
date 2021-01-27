e79499aa446513aa291e27cb2e2cd0288dec0d47
atulvinod <atulvinod1911@gmail.com>
2021-01-27 15:23:28 +0530

    - Created Answer service

---
+++ CorporateQnA.Services/Services/Answer/AnswerService.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using CorporateQnA.Models;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CorporateQnA.Services
+{
+    public class AnswerService : IAnswerService
+    {
+        private readonly PetaPoco.Database database;
+        private readonly IMapper mapper;
+
+        public AnswerService(IConfiguration configuration, AutoMapper.IMapper mapper)
+        {
+            this.mapper = mapper;
+            this.database = new PetaPoco.Database(configuration.GetConnectionString("DB"), "System.Data.SqlClient");
+        }
+
+        public int Create(Answer answer)
+        {
+            try
+            {
+                var data = this.mapper.Map<CorporateQnA.Services.Models.Answer>(answer);
+                var id = this.database.Insert(data);
+                return (int)id;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public IEnumerable<Answer> GetAnswersForQues(string questionId)
+        {
+            var answers = this.database.Query<CorporateQnA.Services.Models.Answer>("SELECT * FROM Answers WHERE QuestionId = @0", questionId).Select(s=>this.mapper.Map<CorporateQnA.Models.Answer>(s));
+            return answers;
+        }
+    }
+}
---
+++ CorporateQnA.Services/Services/Answer/IAnswerService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CorporateQnA.Models;
+
+namespace CorporateQnA.Services
+{
+    public interface IAnswerService
+    {
+        public int Create(Answer answer);
+
+        public IEnumerable<Answer> GetAnswersForQues(string questionId);
+
+    }
+}
